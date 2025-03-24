4e6b2ef29e57539a53b16cdfde090b71c1431b32
Mamush-Meshesha <mamgithub@gmail.com>
2025-03-24 21:45:37 +0300

    jwtkey fixed

--- crypto/jwt.go
+++ crypto/jwt.go
@@ -2,18 +2,24 @@ package crypto
 
 import (
 	"time"
-	"github.com/golang-jwt/jwt/v4"
+
+	"github.com/golang-jwt/jwt/v5" 
 )
 
-var jwtKey = []byte("secretkey")
+// 32-byte secret key for HS256
+var jwtKey = []byte{
+	0x3a, 0x5e, 0x1b, 0x67, 0x9c, 0x2f, 0xa0, 0x8e,
+	0x1f, 0x4d, 0x6b, 0x8a, 0x5c, 0x3e, 0x7d, 0x2b,
+	0x6a, 0x9f, 0x4c, 0x8d, 0x1e, 0x2c, 0x7a, 0x5b,
+	0x3f, 0x6e, 0x8c, 0x9a, 0x7b, 0x2d, 0x4a, 0x1c,
+}
 
-type Claims  struct {
+type Claims struct {
 	UserID uint `json:"user_id"`
 	jwt.RegisteredClaims
 }
 
-
-func GenerateToken (userID uint) (string, error) {
+func GenerateToken(userID uint) (string, error) {
 	expirationTime := time.Now().Add(24 * time.Hour)
 	claims := &Claims{
 		UserID: userID,
@@ -21,7 +27,9 @@ func GenerateToken (userID uint) (string, error) {
 			ExpiresAt: jwt.NewNumericDate(expirationTime),
 		},
 	}
-	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	// Use HS256 for symmetric signing
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) // Changed from ES256 to HS256
 	return token.SignedString(jwtKey)
 }
 
